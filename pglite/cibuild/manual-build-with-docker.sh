fea0c71900ef2375636477177cc1b1e38ae8b166
tudor <tudor@swisstch.com>
2024-12-09 15:25:56 +0100

    added cibuild folder

---
+++ pglite/cibuild/manual-build-with-docker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "======== build-with-dockerl.sh : $(pwd)                 =========="
+echo "======== Building all PGlite prerequisites using Docker =========="
+
+trap 'echo caught interrupt and exiting;' INT
+
+source ./pglite/.buildconfig
+
+if [[ -z "$SDK_VERSION" || -z "$PG_VERSION" ]]; then
+  echo "Missing SDK_VERSION and PG_VERSION env vars."
+  echo "Source them from .buildconfig"
+  exit 1
+fi
+
+IMG_NAME="electricsql/pglite-builder"
+IMG_TAG="${PG_VERSION}_${SDK_VERSION}"
+SDK_ARCHIVE="${SDK_ARCHIVE:-python3.13-wasm-sdk-Ubuntu-22.04.tar.lz4}"
+WASI_SDK_ARCHIVE="${WASI_SDK_ARCHIVE:-python3.13-wasi-sdk-Ubuntu-22.04.tar.lz4}"
+
+docker run \
+  -it \
+  --entrypoint bash\
+  --rm \
+  -e OBJDUMP=${OBJDUMP:-true} \
+  -e SDK_ARCHIVE \
+  -e WASI_SDK_ARCHIVE \
+  -e PGSRC=/workspace/postgres-src\
+  -v ./pglite/cibuild.sh:/workspace/cibuild.sh:rw \
+  -v ./pglite/.buildconfig:/workspace/.buildconfig:rw \
+  -v ./pglite/extra:/workspace/extra:rw \
+  -v ./pglite/cibuild:/workspace/cibuild:rw \
+  -v ./pglite/patches:/workspace/patches:rw \
+  -v ./pglite/tests:/workspace/tests:rw \
+  -v .:/workspace/postgres-src \
+  $IMG_NAME:$IMG_TAG \
+  # bash ./cibuild/build-all.sh
